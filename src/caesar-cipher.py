504777a6dc283bb8039e5de4749605c669ce801c
Yaroslav <yarikmeisan@yandex.com>
2023-01-01 22:44:51 +0300

    Setup letter encrypt with shift

--- src/caesar-cipher.py
+++ src/caesar-cipher.py
@@ -1,9 +1,16 @@
 import string
 
-alphabet = list(string.ascii_uppercase)  # Eng Alphabet
+alphabet = list(string.ascii_lowercase)  # Eng Alphabet
+
 
 def encrypt():
+    originalText = 'a'
     cipher = ''
-    message = ord('A'.upper())
-    print(message)
+    shift = +3
+    for letter in originalText.lower():
+        if letter != ' ':
+            cipher += (chr(ord(letter) + shift))
+    print(cipher)
+
+
 encrypt()
