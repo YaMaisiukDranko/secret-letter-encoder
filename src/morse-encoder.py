1eae764a35a6f3895632a7d48ceb130e77186102
Yaroslav <yarikmeisan@yandex.com>
2023-01-01 21:33:07 +0300

    Setup decrypt, crypt choice

--- src/morse-encoder.py
+++ src/morse-encoder.py
@@ -16,7 +16,7 @@ morse_chars = {'A': '.-', 'B': '-...',
 
 
 def encrypt(message):  # from eng to morse
-    cipher = ''  # variable for cipher
+    cipher = ''  # var for cipher
 
     for letter in message:  # encrypt letter
         if letter != ' ':  # check for space
@@ -25,9 +25,38 @@ def encrypt(message):  # from eng to morse
             cipher += ' '
     return cipher
 
+
+def decrypt(message):  # from morse to eng
+    decipher = ''  # var for deciphering
+    message += ' '
+    citext = ''  # text of morse
+
+    for letter in message:  # decrypt letter
+        if letter != ' ':  # check for space
+            i = 0
+            citext += letter
+        else:
+            i += 1  # new character
+            if i == 2:  # new word
+                decipher += ' '
+            else:
+                decipher += list(morse_chars.keys())[list(morse_chars.values()).index(citext)]
+                citext = ''  # clear
+    return decipher
+
+
 def main():
-    message = 'SOS'  # eng message
-    result = encrypt(message.upper())  # uppercase result
-    print(result)
+    choice = int(input("1  ENG -> MORSE"
+                       "2  MORSE"
+                       "Your choice (1 or 2):  "))
+    if choice == 1:
+        message = 'SOS'  # eng message
+        result = encrypt(message.upper())  # uppercase result
+        print(result)
+    elif choice == 2:
+        message = '... --- ...'
+        result = decrypt(message)
+        print(result)
+
 
 main()
